f9a301259d63904e72ec2d4a953d25f15a45cd06
Nashabhi <akottapalli9@gmail.com>
2026-05-05 23:18:24 -0500

    corrected the demo folder setting file

--- app/django-app/DevOps/DevOps/settings.py
+++ app/django-app/DevOps/DevOps/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,10 +55,11 @@ ROOT_URLCONF = 'DevOps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'demo/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
